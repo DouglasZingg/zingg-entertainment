4103d63e0e5b3e6a635d377e91ed73a7d4e33320
Douglas James Zingg <douglaszingg@gmail.com>
2026-01-28 22:41:18 -0500

    Rewrite pt 3
    Rewrote all pages and reorganized project list

--- js/data.js
+++ js/data.js
@@ -1,13 +1,44 @@
 const projects = [
     {
         id: "graphics-project",
-        title: "Basic Graphic Engine",
+        title: "Graphic Engine",
         summary: "A module graphics engine that diplays a space scene.",
         tech: ["C#"],
-        page: "basic-graphic-engine.html",
+        page: "graphic-engine.html",
         domain: "VFX",
-        featured: true,
+        featured: false,
         github: "https://github.com/DouglasZingg/GraphicsProject"
+    },
+    {
+        id: "max-scene-cleaner",
+        title: "Max Scene Cleaner",
+        summary: "Production style pipeline utility for cleaning and validating 3ds Max scenes before export/hand off.",
+        tech: ["Python"],
+        plateform: "Max",
+        page: "max-scene-cleaner.html",
+        domain: "VFX",
+        featured: false,
+        github: "https://github.com/DouglasZingg/max-scene-cleaner"
+    },
+    {
+        id: "maya-asset-validator",
+        title: "Maya Asset Validator",
+        summary: "A production style validation and auto fix tool for Autodesk Maya (2020-2026) built with Python and Qt.",
+        tech: ["Python", "Maya"],
+        page: "maya-asset-validator.html",
+        domain: "VFX",
+        featured: false,
+        github: "https://github.com/DouglasZingg/maya-asset-validator"
+    },
+    {
+        id: "pipeline-delivery-packager",
+        title: "Pipeline Devlivery Packager",
+        summary: "Standalone desktop pipeline utility that validates and packages an asset delivery into a clean 'studio drop' layout, then exports a manifest.json and an HTML report.",
+        tech: ["Python"],
+        page: "pipeline-delivery-packager.html",
+        domain: ["VFX", "Game Development"], 
+        featured: false,
+        github: "https://github.com/DouglasZingg/pipeline-delivery-packager"
     },
     {
         id: "reactionary",
@@ -21,17 +52,6 @@ const projects = [
         github: "https://github.com/DouglasZingg/Reactionary-Final-Build"
     },
     {
-        id: "tormented-souls",
-        title: "Tormented Souls",
-        summary: "Horror survival game",
-        tech: ["C#"],
-        plateform: "Unity",
-        page: "tormented-souls.html",
-        domain: "Game Development",
-        featured: true,
-        github: "https://github.com/DouglasZingg/TormentedSouls"
-    },
-    {
         id: "shotgrid-mini-publisher",
         title: "Shotgrid Mini Publisher",
         summary: "A standalone PySide6 desktop tool that connects to Flow Production Tracking (ShotGrid).",
@@ -43,73 +63,56 @@ const projects = [
         github: "https://github.com/DouglasZingg/shotgrid-mini-publisher"
     },
     {
+        id: "studio-folder-builder",
+        title: "Studio Folder Builder",
+        summary: "A standalone Python ans PySide6 tool that generates studio style project folder structures from configurable templates.",
+        tech: ["Python"],
+        page: "studio-folder-builder.html",
+        domain: "VFX",
+        featured: false,
+        github: "https://github.com/DouglasZingg/studio-folder-builder"
+    },
+    {
+        id: "texture-pack-validator",
+        title: "Texture Pack Validator",
+        summary: "Standalone Python and PySide6 utility that validates Substance Painter or game ready texture exports and produces severity based QA reports.",
+        tech: ["Python"],
+        plateform: ["Unity", "Unreal"],
+        page: "texture-pack-validator.html",
+        domain: ["VFX" ,"Game Devlopment"],
+        featured: false,
+        github: "https://github.com/DouglasZingg/texture-pack-validator"
+    },
+    {
+        id: "tormented-souls",
+        title: "Tormented Souls",
+        summary: "Horror survival game",
+        tech: ["C#"],
+        plateform: "Unity",
+        page: "tormented-souls.html",
+        domain: "Game Development",
+        featured: false,
+        github: "https://github.com/DouglasZingg/TormentedSouls"
+    },
+    {
+        id: "ue-asset-prep-tool",
+        title: "UE Asset Prep Tool",
+        summary: "Production style Unreal Engine Editor Utility Widget for validating and preparing assets for game/film/mobile pipelines.",
+        tech: ["Python"],
+        plateform: "Max",
+        page: "ue-asset-prep-tool.html",
+        domain: ["VFX", "Game Development"], 
+        featured: false,
+        github: "https://github.com/DouglasZingg/ue-asset-prep-tool"
+    },
+    {
         id: "usd-inspector-packager",
         title: "USD Inspsctor Packager",
         summary: "Standalone Python desktop app for inspecting USD assets and packaging dependencies into a portable drop.",
         tech: ["Python"],
         page: "usd-inspector-packager.html",
         domain: "VFX",
-        featured: true,
+        featured: false,
         github: "https://github.com/DouglasZingg/usd-inspector-packager"
-    },
-    {
-        id: "texture-pack-validator",
-        title: "Texture Pack Validator",
-        summary: "Standalone Python and PySide6 utility that validates Substance Painter or game-ready texture exports and produces severity-based QA reports.",
-        tech: ["Python", "Unreal", "Unity"],
-        page: "texture-pack-validator.html",
-        domain: ["VFX" ,"Game Devlopment"],
-        featured: true,
-        github: "https://github.com/DouglasZingg/texture-pack-validator"
-    },
-    {
-        id: "studio-folder-builder",
-        title: "Studio Folder Builder",
-        summary: "A standalone Python + PySide6 tool that generates studio-style project folder structures from configurable templates.",
-        tech: ["Python"],
-        page: "studio-folder-builder.html",
-        domain: "VFX",
-        featured: true,
-        github: "https://github.com/DouglasZingg/studio-folder-builder"
-    },
-    {
-        id: "maya-asset-validator",
-        title: "Maya Asset Validator",
-        summary: "A production-style validation and auto-fix tool for Autodesk Maya (2020–2026) built with Python and Qt.",
-        tech: ["Python", "Maya"],
-        page: "maya-asset-validator.html",
-        domain: "VFX",
-        featured: true,
-        github: "https://github.com/DouglasZingg/maya-asset-validator"
-    },
-    {
-        id: "pipeline-delivery-packager",
-        title: "Pipeline Devlivery Packager",
-        summary: "Standalone desktop pipeline utility that validates and packages an asset delivery into a clean 'studio drop' layout, then exports a manifest.json and an HTML report.",
-        tech: ["Python"],
-        page: "pipeline-delivery-packager.html",
-        domain: ["VFX", "Game Development"], 
-        featured: true,
-        github: "https://github.com/DouglasZingg/pipeline-delivery-packager"
-    },
-    {
-        id: "max-scene-cleaner",
-        title: "Max Scene Cleaner",
-        summary: "Production-style pipeline utility for cleaning and validating 3ds Max scenes before export/hand-off.",
-        tech: ["Python", "Max"],
-        page: "max-scene-cleaner.html",
-        domain: "VFX",
-        featured: true,
-        github: "https://github.com/DouglasZingg/max-scene-cleaner"
-    },
-    {
-        id: "ue-asset-prep-tool",
-        title: "UE Asset Prep Tool",
-        summary: "Production-style pipeline utility for cleaning and validating 3ds Max scenes before export/hand-off.",
-        tech: ["Python", "Max"],
-        page: "ue-asset-prep-tool.html",
-        domain: ["VFX", "Game Development"], 
-        featured: true,
-        github: "https://github.com/DouglasZingg/ue-asset-prep-tool"
     },
 ];
